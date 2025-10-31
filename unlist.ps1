705ed417c76eecb08282e0da406a8db846db3231
LoWang-Test <way.of.wang@outlook.com>
2025-10-31 12:50:10 +0000

    unlist

---
+++ unlist.ps1
@@ -0,0 +1,5 @@
+param($key)
+
+for ($i = 1; $i -le 557; $i++) {
+    dotnet nuget delete "LimitTester.Package$($i.ToString("0000")).1.0.0.nupkg" --api-key $key --source https://api.nuget.org/v3/index.json --non-interactive --yes 
+}
